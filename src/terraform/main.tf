238e62d361b268c5f3ce39444bced548a8717541
Bryce Palmer <everettraven@gmail.com>
2020-07-12 13:56:51 -0400

    Added variables and reorganized

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,7 +1,40 @@
 provider "google" {
-    # credentials = file("credentials.json") //replace this with your credentials file
-    # project = "flower-gan" //replace this with your project name
-    # region = "us-central1"
+
+}
+
+variable "machine_type" {
+    type = string
+    default = "n1-standard-8"
+}
+
+variable "guest_accelerator_type" {
+    type = string
+    default = "nvidia-tesla-k80"
+}
+
+variable "guest_accelerator_count" {
+    type = number
+    default = 1
+}
+
+variable "disk_size" {
+    type = number
+    default = 100
+}
+
+variable "machine_username" {
+    type = string
+    default = "username"
+}
+
+variable "jupyter_port" {
+    type = string
+    default = "8888"
+}
+
+variable "ssh_key_path" {
+    type = string
+    default = "~/.ssh/id_rsa.pub"
 }
 
 //create a random id to append to our compute instance name
@@ -10,21 +43,20 @@ resource "random_id" "instance_id" {
 }
 
 resource "google_compute_instance" "default" {
-    name = "stock-prediction-${random_id.instance_id.hex}"
-    machine_type = "n1-standard-8"
-    # zone = "us-central1-a"
+    name = "ml-automated-${random_id.instance_id.hex}"
+    machine_type = var.machine_type
 
     //initialize our guest accelerator(s)
     guest_accelerator {
-        type = "nvidia-tesla-k80"
-        count = 1
+        type = var.guest_accelerator_type
+        count = var.guest_accelerator_count
     }
 
     //initialize our boot disk with the image we want on it
     boot_disk {
         initialize_params {
             image = "ubuntu-os-cloud/ubuntu-1804-lts"
-            size = 100
+            size = var.disk_size
         }
     }
 
@@ -40,7 +72,7 @@ resource "google_compute_instance" "default" {
 
     //create our user here with ssh keys as the way to log in
     metadata = {
-        ssh-keys = "mrpalmer:${file("~/.ssh/id_rsa.pub")}"
+        ssh-keys = "${var.machine_username}:${file(var.ssh_key_path)}"
     }
 
     //set on_host_maintenance to TERMINATE as guest accelerators can't be used when it is set to MIGRATE
@@ -51,12 +83,12 @@ resource "google_compute_instance" "default" {
 
 //create our firewall rule to allow access to a Jupyter server running on our VM
 resource "google_compute_firewall" "default" {
-    name = "stock-prediction-${random_id.instance_id.hex}-firewall"
+    name = "ml-automated-jupyter-${random_id.instance_id.hex}-firewall"
     network = "default"
 
     allow {
         protocol = "tcp"
-        ports = ["8888"] //running Jupyter on port 8888, change this if youd like
+        ports = [var.jupyter_port]
     }
 }
 
